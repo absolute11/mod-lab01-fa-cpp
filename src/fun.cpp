37616d4ac94a1bec6c9da56c6823713e8e4ece41
fortuna <den.gitelman@gmail.com>
2026-04-02 18:54:00 +0300

    b6 Гительман Денис

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,76 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+    bool hasDigit = false;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            if (!inWord) {
+                inWord = true;
+                hasDigit = false;
+            }
+            if (str[i] >= '0' && str[i] <= '9')
+                hasDigit = true;
+        } else {
+            if (inWord) {
+                if (!hasDigit) count++;
+                inWord = false;
+            }
+        }
+    }
+    if (inWord && !hasDigit) count++;
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+    bool valid = false;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            if (!inWord) {
+                inWord = true;
+                valid = (str[i] >= 'A' && str[i] <= 'Z');
+            } else {
+                if (valid && !(str[i] >= 'a' && str[i] <= 'z'))
+                    valid = false;
+            }
+        } else {
+            if (inWord) {
+                if (valid) count++;
+                inWord = false;
+            }
+        }
+    }
+    if (inWord && valid) count++;
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    unsigned int totalLen = 0;
+    unsigned int wordCount = 0;
+    bool inWord = false;
+    unsigned int curLen = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            if (!inWord) {
+                inWord = true;
+                curLen = 0;
+            }
+            curLen++;
+        } else {
+            if (inWord) {
+                totalLen += curLen;
+                wordCount++;
+                inWord = false;
+            }
+        }
+    }
+    if (inWord) {
+        totalLen += curLen;
+        wordCount++;
+    }
+    if (wordCount == 0) return 0;
+    return (totalLen + wordCount / 2) / wordCount;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
+    const char* str1 = "apple 123abc banana 456 cherry";
+    std::cout << "Слова без цифр: " << faStr1(str1) << std::endl;
+
+    const char* str2 = "Hello world Nice Day bad";
+    std::cout << "Слова с заглавной: " << faStr2(str2) << std::endl;
+
+    const char* str3 = "the quick brown car";
+    std::cout << "Средняя длина слова: " << faStr3(str3) << std::endl;
+
     return 0;
 }
